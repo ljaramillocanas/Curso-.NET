9be0c60ded1896459b1e993a6764bf3fb54ad5f7
Luis JaramillO <ljaramillocanas@gmail.com>
2024-03-18 19:12:28 -0500

    Se agrega ciclo anidado for

---
+++ Ciclos-anidados/Ciclos-anidados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos_anidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaramos variables 
+             * i para bucle exterior
+             * j para bucle interno
+            */
+
+            int i, j, resultado;
+            
+            for(i=1; i <= 10; i++)
+            {
+                Console.WriteLine("Tabla multiplicar del {0}",i);
+                for(j=1; j <= 10; j++)
+                {
+                    resultado = i*j;
+                    Console.WriteLine("{0} * {1} = {2} ",i,j,resultado);
+                }
+
+            }
+
+        }
+    }
+}
