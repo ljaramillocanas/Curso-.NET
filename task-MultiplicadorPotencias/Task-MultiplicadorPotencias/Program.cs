ccda4ad4c41e38cb7f77efa8c05ba01b811a1db0
Luis JaramillO <ljaramillocanas@gmail.com>
2024-03-18 20:16:12 -0500

    Se agrega task de Multiplicador de potencias

---
+++ task-MultiplicadorPotencias/Task-MultiplicadorPotencias/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_MultiplicadorPotencias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos aplicativo de consola el cual permita multiplicar un número dado por teclado por una potencia dada por teclado
+
+            // Declaramos variables
+
+            int numero, exponente, i;
+            double resultado = 1, resultadoNegativo;
+
+            //Solicitamos números
+            Console.WriteLine("Digita el número base que desea potenciar");
+            numero = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Digita la potencia");
+            exponente = Convert.ToInt32(Console.ReadLine());
+
+            //Iniciamos proceso validando exponente 
+            if(exponente < 0)
+            {
+                exponente *= -1; //convertimos a positivo
+                for (i = 1; i <= exponente; i++)
+                {
+                    resultado *= numero; 
+                }
+                resultadoNegativo = 1/resultado;
+                Console.WriteLine("{0}^{1} = {2} ", numero, exponente, resultadoNegativo);
+            }else
+            {
+                for (i = 1; i <= exponente; i++)
+                {
+                    resultado *= numero;
+                }
+                Console.WriteLine("{0}^{1} = {2} ", numero, exponente, resultado);
+            }
+
+
+        }
+    }
+}
