35ce82d04550734eab978aeffb0dd6a8a5487f7d
Luis JaramillO <ljaramillocanas@gmail.com>
2024-03-20 18:56:39 -0500

    Se actualizan matrices con ejercicio pratico de ingreso de datos y lectura

--- Matrices/Matrices/Program.cs
+++ Matrices/Matrices/Program.cs
@@ -10,16 +10,29 @@ namespace Matrices
     {
         static void Main(string[] args)
         {
-
+            byte tamaño;
+            double promedio, acomulador=0;
+            Console.WriteLine("Ingrese la cantidad de calificaciones a promediar");
+            tamaño = Convert.ToByte(Console.ReadLine());
             //Estructura de matriz
             // tipo [] nombre = new tipo [tamaño]
-
-            double[] calificaciones = new double[4];
-            string[] nombres = new string[5];
-            int[] numeros = new int[10];
-            byte[] datos = new byte[7];
-
-
+            // Creamos un arreglo de calificaciones con el tamaño especificado por el usuario
+            double[] calificaciones = new double[tamaño];
+            // Solicitamos al usuario que ingrese las calificaciones
+            for (int i=0; i < calificaciones.Length; i++) { 
+            Console.WriteLine("ingrese la calificación #{0}", i + 1);
+                calificaciones[i]= Convert.ToDouble(Console.ReadLine());
+                acomulador += calificaciones[i];
+        }
+            // Mostramos las calificaciones ingresadas por el usuario
+            Console.WriteLine("Calificaciones ingresadas:");
+            for (int i = 0; i < calificaciones.Length; i++)
+            {
+                Console.WriteLine("Calificación #{0}: {1}", i + 1, calificaciones[i]);
+            }
+            promedio = acomulador / tamaño;
+            Console.WriteLine("EL promedio del estudiante es {0}",promedio);
+            
         }
     }
 }
