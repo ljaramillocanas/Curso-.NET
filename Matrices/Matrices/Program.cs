89542147535bc5e185446a9ffd199100dc94e6d8
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-01 18:24:24 -0500

    Actulización de curso

--- Matrices/Matrices/Program.cs
+++ Matrices/Matrices/Program.cs
@@ -10,28 +10,34 @@ namespace Matrices
     {
         static void Main(string[] args)
         {
-            byte tamaño;
-            double promedio, acomulador=0;
+            byte cantidadCal,cantidadAlumnos;
+            double  promedio, acomulador=0;
+            Console.WriteLine("Ingrese la cantidad de alumnos a promediar");
+            cantidadAlumnos = Convert.ToByte(Console.ReadLine());
             Console.WriteLine("Ingrese la cantidad de calificaciones a promediar");
-            tamaño = Convert.ToByte(Console.ReadLine());
+            cantidadCal = Convert.ToByte(Console.ReadLine());
             //Estructura de matriz
             // tipo [] nombre = new tipo [tamaño]
             // Creamos un arreglo de calificaciones con el tamaño especificado por el usuario
-            double[] calificaciones = new double[tamaño];
+            double[,] calificaciones = new double[cantidadAlumnos,cantidadCal];
             // Solicitamos al usuario que ingrese las calificaciones
-            for (int i=0; i < calificaciones.Length; i++) { 
-            Console.WriteLine("ingrese la calificación #{0}", i + 1);
-                calificaciones[i]= Convert.ToDouble(Console.ReadLine());
-                acomulador += calificaciones[i];
+            for (int i=0; i < cantidadAlumnos; i++) { 
+                for (int j=0; j < cantidadCal; j++) { 
+            Console.WriteLine("ingrese la calificación {0} del estudiante #{1}",j+1, i + 1);
+                calificaciones[i,j]= Convert.ToDouble(Console.ReadLine());
+                acomulador += calificaciones[i,j];
         }
+            } 
             // Mostramos las calificaciones ingresadas por el usuario
             Console.WriteLine("Calificaciones ingresadas:");
-            for (int i = 0; i < calificaciones.Length; i++)
+            for (int i = 0; i < cantidadAlumnos; i++)
             {
-                Console.WriteLine("Calificación #{0}: {1}", i + 1, calificaciones[i]);
+                for (int j=0;j < cantidadCal; j++) { 
+                Console.WriteLine("Calificación #{2} del estudiante #{0}: {1}", i + 1, calificaciones[i,j],j+1);
+                }
             }
-            promedio = acomulador / tamaño;
-            Console.WriteLine("EL promedio del estudiante es {0}",promedio);
+            promedio = acomulador / ( cantidadAlumnos * cantidadCal);
+            Console.WriteLine("EL promedio de los estudiante es {0}",promedio);
             
         }
     }
---
+++ PracticaMatrizMultidemensional/PracticaMatrizMultidemensional/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    // Clase Program que contiene el método Main, punto de entrada del programa
+    internal class Program
+    {
+        // Método principal del programa
+        static void Main(string[] args)
+        {
+            // Declaración de variables para almacenar la cantidad de calificaciones y alumnos
+            byte cantidadCal, cantidadAlumnos;
+            double promedio, acumulador = 0; // Declara las variables para el promedio y la sumatoria de calificaciones
+
+            // Solicita al usuario que ingrese la cantidad de alumnos a promediar
+            Console.WriteLine("Ingrese la cantidad de alumnos a promediar");
+            cantidadAlumnos = Convert.ToByte(Console.ReadLine());
+
+            // Solicita al usuario que ingrese la cantidad de calificaciones a promediar
+            Console.WriteLine("Ingrese la cantidad de calificaciones a promediar");
+            cantidadCal = Convert.ToByte(Console.ReadLine());
+
+            // Declara una matriz para almacenar las calificaciones de los alumnos
+            double[,] calificaciones = new double[cantidadAlumnos, cantidadCal];
+
+            // Solicita al usuario que ingrese las calificaciones
+            for (int i = 0; i < cantidadAlumnos; i++)
+            {
+                for (int j = 0; j < cantidadCal; j++)
+                {
+                    // Solicita al usuario que ingrese la calificación del estudiante
+                    Console.WriteLine("Ingrese la calificación {0} del estudiante #{1}", j + 1, i + 1);
+                    // Almacena la calificación ingresada por el usuario en la matriz de calificaciones
+                    calificaciones[i, j] = Convert.ToDouble(Console.ReadLine());
+                    // Calcula la sumatoria de calificaciones
+                    acumulador += calificaciones[i, j];
+                }
+            }
+
+            // Muestra las calificaciones ingresadas por el usuario
+            Console.WriteLine("Calificaciones ingresadas:");
+            for (int i = 0; i < cantidadAlumnos; i++)
+            {
+                for (int j = 0; j < cantidadCal; j++)
+                {
+                    // Muestra la calificación de cada estudiante
+                    Console.WriteLine("Calificación #{2} del estudiante #{0}: {1}", i + 1, calificaciones[i, j], j + 1);
+                }
+            }
+
+            // Calcula el promedio de todas las calificaciones ingresadas
+            promedio = acumulador / (cantidadAlumnos * cantidadCal);
+            // Muestra el promedio de los estudiantes
+            Console.WriteLine("El promedio de los estudiantes es {0}", promedio);
+        }
+    }
+}
