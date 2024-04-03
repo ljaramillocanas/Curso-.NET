5b1b435b82fbfb8987614b10c4e6632009d9c5aa
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-02 20:40:03 -0500

    Se realizan cambios

--- MatricesEscalonadas/MatricesEscalonadas/Program.cs
+++ MatricesEscalonadas/MatricesEscalonadas/Program.cs
@@ -15,45 +15,59 @@ namespace MatricesEscalonadas
             double totalVentas = 0;
             byte sem, day,semAct,venPorSem;
             int i, j;
-
-            Console.WriteLine("Ingrese la cantidad de semanas que estuvo activo ");
+            Console.WriteLine("**** BIENVENIDO AL CALCULADOR DE VENTAS POR SEMANA****");
+            Console.WriteLine("");
+            Console.WriteLine("***Ingrese la cantidad de semanas que estuvo activo***");
             semAct = Convert.ToByte(Console.ReadLine());
             // Creación del arreglo bidimensional para almacenar las ventas
             double[][] ventas = new double[semAct][];
+            Console.WriteLine("");
+            
             // Registro de las ventas por semana
             for ( i = 0; i < semAct; i++)
             {
-                Console.WriteLine("Ingrese la cantidad de ventas realizadas en la semana {0} ", i + 1);
+                
+                Console.WriteLine("***Ingrese la cantidad de ventas realizadas en la semana {0} *** ", i + 1);
                 venPorSem = Convert.ToByte(Console.ReadLine());
                 ventas[i]=new double[venPorSem];
             }
             
-
             for (i = 0;i < ventas.Length; i++)
+            
             {
-                for( j=0;j < ventas[i].Length;j++){
-                    Console.WriteLine("ingrese la venta #{0} de la semana #{1}",j+1,i+1);
+                Console.WriteLine("");
+                Console.WriteLine(" **--** VENTAS SEMANA {0}**-- ** ", i + 1);
+                Console.WriteLine("");
+                for ( j=0;j < ventas[i].Length;j++){
+                    
+                    Console.WriteLine("* Ingrese la venta #{0} *",j+1,i+1);
                     ventas[i][j] = Convert.ToDouble(Console.ReadLine());
                     totalVenSem += ventas[i][j];
                     
                 }
-                Console.WriteLine("El total vendido en la semana {0} es {1}$",i+1,totalVenSem);
+                Console.WriteLine("");
+                Console.WriteLine("** El total vendido en la semana {0} es {1}$",i+1,totalVenSem);
                 totalVentas += totalVenSem;
                 totalVenSem = 0;
             
             }
-            Console.WriteLine("El valor total del lo vendido es {0}$", totalVentas);
+            Console.WriteLine("");
+            Console.WriteLine("**El valor total del lo vendido es {0}$", totalVentas);
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("ingrese la semana de la venta que desea consultar");
-            sem = Convert.ToByte(Console.ReadLine());   
+            sem = Convert.ToByte(Console.ReadLine());
             Console.WriteLine("ingrese el número de la venta que desea consultar");
             day = Convert.ToByte(Console.ReadLine());
             // Validación de la semana y el número de venta
             if (sem <= 0 || sem > semAct || day <= 0 || day > ventas[sem - 1].Length)
             {
+                Console.WriteLine("");
                 Console.WriteLine("Error: Semana o número de venta inválido.");
             }
             else
             {
+                Console.WriteLine("");
                 Console.WriteLine("Venta {0} de la semana {1}: ${2}", day, sem, ventas[sem - 1][day - 1]);
             }
         }
---
+++ MetodosMatrices/MetodosMatrices/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matriz = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            Console.WriteLine("Los valores de la matriz unidimensional son :");
+
+            ImprimirMatriz(matriz);
+
+            double[,] matriz2D = { { 1,4,7 },
+                                    {5,8,9 },
+                                    {7,5,10 } };
+
+            Console.WriteLine("Los valores de la matriz multidimensional  son :");
+            ImprimirMatriz2D(matriz2D);
+            Console.WriteLine(matriz2D.GetLength(1));
+
+        }
+
+        //Metodo para imprimir matriz 
+
+        static void ImprimirMatriz(int[] matrizPa)
+        {
+            for (int i = 0; i < matrizPa.Length; i++)
+            {
+                Console.WriteLine(matrizPa[i]);
+            }
+        }
+        static void ImprimirMatriz2D(double[,] matriz2DPa)
+        {
+            for (int i = 0; i < matriz2DPa.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz2DPa.GetLength(1); j++) 
+                { 
+                Console.WriteLine(matriz2DPa[i,j]);
+                }
+            }
+        }
+    }
+}
