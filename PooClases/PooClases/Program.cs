411507360e7634af3ca5b0706044dc7b3bcaa831
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-03 19:23:04 -0500

    Se actualiza seción con invocación de metodos

--- PooClases/PooClases/Program.cs
+++ PooClases/PooClases/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,42 @@ namespace PooClases
      class Program
     {
         static void Main(string[] args)
+
         {
-            Motocicleta cuatrimoto = new Motocicleta();
-            cuatrimoto.año = 2025;
-            cuatrimoto.llantas = 4;
-            Console.WriteLine("El modelo de la moto es : {0}",cuatrimoto.año);
-            cuatrimoto.frenar();
+            Console.WriteLine("Seleccione el VH que desea consultar");
+            Console.WriteLine("1. Cuatrimoto");
+            Console.WriteLine("2. Enduro ");
+
+            int sel = Convert.ToInt32(Console.ReadLine());  
+            if(sel == 1) { 
+
+                Motocicleta cuatrimoto = new Motocicleta();
+                cuatrimoto.año = 2025;
+                cuatrimoto.llantas = 4;
+                cuatrimoto.combustible = "ACP";
+                Console.WriteLine("*** CUATRIMOTO *** ");
+                Console.WriteLine("El modelo de la cuatrimoto es : {0}", cuatrimoto.año);
+                Console.WriteLine("El combustible de la cuatrimoto es : {0}", cuatrimoto.combustible);
+                cuatrimoto.frenar();
+                Console.WriteLine("La cantidad de llantas del VH : {0}", cuatrimoto.llantas);
+            }
+            if (sel == 2 ) {  
+                Motocicleta Enduro = new Motocicleta();
+                Enduro.año = 2024;
+                Enduro.llantas = 2;
+                Enduro.combustible = "Extra";
+            
+                Console.WriteLine("*** ENDURO *** ");
+                Enduro.acelar();
+                Console.WriteLine("El modelo de la enduro  es : {0}", Enduro.año);
+                Console.WriteLine("El combustible de la enduro es : {0}", Enduro.combustible);
+                Console.WriteLine("La cantidad de llantas de la enduro  es : {0}", Enduro.llantas);
+            }
         }
         
     }
+    
+    
     // Se crea clase Motocicleta
     public class Motocicleta
     {
