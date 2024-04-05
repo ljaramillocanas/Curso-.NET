a85d569a678749cb0a1c91f11f1b9330c927c3db
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-04 19:50:07 -0500

    Se crea metodos y metodo sobre cargado, se crea opcion para ingresar nombre de usuario

--- PooClases/PooClases/Program.cs
+++ PooClases/PooClases/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -13,20 +14,30 @@ namespace PooClases
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Seleccione el VH que desea consultar");
+            Motocicleta motocicleta = new Motocicleta();
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su primer apellido");
+            string apellido = Console.ReadLine();
+            String NombreC = motocicleta.Concatenar(nombre, apellido);
+            Console.WriteLine("Sr.(a) {0} Ingrese el VH que dese consultar",NombreC);
             Console.WriteLine("1. Cuatrimoto");
             Console.WriteLine("2. Enduro ");
+            
+            
 
             int sel = Convert.ToInt32(Console.ReadLine());  
             if(sel == 1) { 
 
                 Motocicleta cuatrimoto = new Motocicleta();
+                
                 cuatrimoto.Año = "2025";
                 cuatrimoto.Llantas = 4;
                 cuatrimoto.Combustible = "ACPM";
                 cuatrimoto.NumPuertas = "N/A";
                 cuatrimoto.CCmotor = 1600;
                 cuatrimoto.Color = "Rojo";
+                cuatrimoto.Cajuela = true;
                 
                 Console.WriteLine(cuatrimoto.ToString());
             }
@@ -52,8 +63,14 @@ namespace PooClases
     {
         //Campos
         private string color, combustible, año, numPuertas;
-        private int ccMotor, llantas;
+        private int ccMotor, llantas, nPasajeros;
+        private bool cajuela;
+        public bool Cajuela
+        {
+            get { return cajuela; }
+            set { cajuela = value; }
 
+        }
         public string Año
         {
             get => año;
@@ -111,12 +128,37 @@ namespace PooClases
             velocidadesPa++;
             Console.WriteLine("Cambiando la velocidad");
         }
-       
+
+        //Contructor de la clase
+        public Motocicleta()
+        {
+
+            nPasajeros = 2;
+            cajuela = false;
+
+        }
+
         //invalidando el metodo ToString
         public override string ToString()
         { 
-           string mensaje = "[Modelo : " + año + "] [Color : " + color + "] [Puertas : "+ numPuertas + "] [Combustible : "+ combustible + "] [Llantas : "+llantas  +"]";
+           string mensaje = "[Modelo : " + año + "] [Color : " + color + "] [Puertas : "+ numPuertas + "] "+ "\n[Combustible : "+ combustible + "] [Llantas : "+llantas  +"] [cajuela: " + cajuela +"]" ;
             return mensaje;
+        }
+
+        //Metodo para concatenar nombres
+        public string Concatenar(string name,string lastname)
+        {
+            string nombreCompleto = name + lastname;
+
+            return nombreCompleto;
+        }
+
+        //Sobrecarga del metodo para concatenar nombres
+        public string Concatenar(string name, string lastName, string lastName2)
+        {
+            string nombreCompleto = name + "" + lastName + "" + lastName2;
+
+            return nombreCompleto;
         }
 
     }
