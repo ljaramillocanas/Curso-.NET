f146548652d8710a026c011c91d6bffccee0ee19
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-16 18:36:21 -0500

    Se agrega metodo add y remove a la practica con listas.

--- ColeccionesList/ColeccionesList/Program.cs
+++ ColeccionesList/ColeccionesList/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace ColeccionesList
         {
             string elem;
             int elemns;
-            int opc;
+            int opc,pos;
 
             List<string> personas = new List<string>();
 
@@ -20,10 +21,13 @@ namespace ColeccionesList
 
             do
             {
-                Console.WriteLine("1. Agregar elementos");
-                Console.WriteLine("2. Mostrar elementos por selección");
-                Console.WriteLine("3. Mostrar todos los elementos");
-                Console.WriteLine("4. Salir");
+                Console.WriteLine("BIENVENIDO AL ADMINISTRADOR DE LISTAS \n");
+                Console.WriteLine("1. Agregar elementos en orden");
+                Console.WriteLine("2. Agregar elementos por posicion");
+                Console.WriteLine("3. Quitar elemento por posición ");
+                Console.WriteLine("4. Mostrar elementos por selección");
+                Console.WriteLine("5. Mostrar todos los elementos");
+                Console.WriteLine("6. Salir");
                 opc = Convert.ToInt32(Console.ReadLine());
                 switch (opc)
                 {
@@ -33,20 +37,40 @@ namespace ColeccionesList
                         personas.Add(elem);
                         break;
                     case 2:
+                        Console.WriteLine("Ingrese en que posición desea ingresar el elemento");
+                        pos = Convert.ToInt32(Console.ReadLine())-1;
+                        Console.WriteLine("Ingrese el elemento a almacenar: ");
+                        elem = Console.ReadLine();
+                        personas.Insert(pos, elem);
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese de que posición desea quitar el elemento");
+                        pos = Convert.ToInt32(Console.ReadLine())-1;
+                        personas.RemoveAt(pos);
+                        break;
+                    case 4:
                         Console.WriteLine("Ingrese el elemento a mostrar: ");
                         elemns = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine(personas[elemns]);
                         break;
-                    case 3:
+                    case 5:
                         Console.WriteLine("Todos los elementos son: ");
                         for (int i = 0; i < personas.Count; i++)
                         {
                             Console.WriteLine("Elemento {0} : {1}",i+1,personas[i]);
                         }
+                        Console.WriteLine("Todos los elementos con foreach son: ");
+                        foreach (string elemento in personas)
+                        {
+                            Console.WriteLine(elemento);
+                        }
                         break;
-                }
-            }while (opc!=);
 
+                        
+                }
+            }while (opc!=6);
+           
         }
+       
     }
 }
