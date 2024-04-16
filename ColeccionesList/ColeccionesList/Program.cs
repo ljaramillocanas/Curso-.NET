c030e0a6b4c6e12d1e945daa8ef2fc3e8c78dc77
Luis JaramillO <ljaramillocanas@gmail.com>
2024-04-16 18:06:42 -0500

    Se agrega pratica con List, como agregar, y mostrar elementos.

---
+++ ColeccionesList/ColeccionesList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string elem;
+            int elemns;
+            int opc;
+
+            List<string> personas = new List<string>();
+
+            // agregar obj en lista
+
+            do
+            {
+                Console.WriteLine("1. Agregar elementos");
+                Console.WriteLine("2. Mostrar elementos por selección");
+                Console.WriteLine("3. Mostrar todos los elementos");
+                Console.WriteLine("4. Salir");
+                opc = Convert.ToInt32(Console.ReadLine());
+                switch (opc)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el elemento a almacenar: ");
+                        elem = Console.ReadLine();  
+                        personas.Add(elem);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese el elemento a mostrar: ");
+                        elemns = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(personas[elemns]);
+                        break;
+                    case 3:
+                        Console.WriteLine("Todos los elementos son: ");
+                        for (int i = 0; i < personas.Count; i++)
+                        {
+                            Console.WriteLine("Elemento {0} : {1}",i+1,personas[i]);
+                        }
+                        break;
+                }
+            }while (opc!=);
+
+        }
+    }
+}
